47402f8e81ce3710ecd946fd441b2391f4d3bc77
fregante <me@fregante.com>
2025-01-20 23:19:24 +0100

    Delete global.d.ts

--- global.d.ts
+++
@@ -1,4 +0,0 @@
-declare namespace chrome.contextMenus {
-	// export function update(id: string | number, updateProperties: UpdateProperties): Promise<void>;
-	// export function create(createProperties: CreateProperties): Promise<void>;
-}
